478a4ce037c2ca72101e241289612e1d8c73268e
Senou Kounouho <sckounouho@gmail.com>
2026-04-19 18:24:29 -0700

    refactor(home): use PageContainer (65ch)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { getAboutPage } from "@/lib/content";
 import { renderMdx } from "@/lib/content/mdx";
+import { PageContainer } from "@/components/layout/PageContainer";
 
 export default function Home() {
   const about = getAboutPage();
   return (
-    <div className="mx-auto max-w-[60ch] px-6 pt-24 pb-24 md:pt-[20vh]">
+    <PageContainer>
       <article className="prose-site w-full">{renderMdx(about.body)}</article>
-    </div>
+    </PageContainer>
   );
 }
